a36bc5b9e7cc86d2640fff7c2b646228743f2369
jas- <jason.gerfen@gmail.com>
2012-06-01 08:21:26 -0600

    Fri Jun  1 08:21:26 MDT 2012 - Latest patch

--- openssl-spki-cli.php
+++ openssl-spki-cli.php
@@ -2,173 +2,57 @@
 <?php
 
 echo "Generating private key...";
-$key = openssl_pkey_new(array('digest_alg' => 'sha512',
-                              'private_key_type' => OPENSSL_KEYTYPE_RSA,
-                              'private_key_bits' => 2048));
+$pkey = openssl_pkey_new(array('digest_alg' => 'sha512',
+                               'private_key_type' => OPENSSL_KEYTYPE_RSA,
+                               'private_key_bits' => 2048));
+openssl_pkey_export($pkey, $pass);
 echo "done\n";
 echo "============================\n";
 
-echo "Creating SPKAC using md5 signature...\n";
-if (function_exists('openssl_spki_new')){
- $spki = openssl_spki_new($key, _uuid(), 'md5');
- echo $spki;
+$algo = array('md4'=>OPENSSL_ALGO_MD4,
+              'md5'=>OPENSSL_ALGO_MD5,
+              'sha1'=>OPENSSL_ALGO_SHA1,
+              'sha256'=>OPENSSL_ALGO_SHA256,
+              'sha384'=>OPENSSL_ALGO_SHA384,
+              'sha512'=>OPENSSL_ALGO_SHA512,
+              'ripemd160'=>OPENSSL_ALGO_RIPEMD160);
+
+foreach($algo as $key => $value) {
+
+	echo "Creating SPKAC using ".$key." signature...\n";
+	if (function_exists('openssl_spki_new')){
+		$spki = openssl_spki_new($pkey, _uuid(), $value);
+		echo $spki;
+	}
+	echo "\ndone\n";
+	echo "============================\n";
+
+	echo "Verifying SPKAC with ".$key." signature...\n";
+	if (function_exists('openssl_spki_verify')){
+		$w = openssl_spki_verify(preg_replace('/SPKAC=/', '', $spki));
+		var_dump($w);
+	}
+	echo "\n============================\n";
+
+	echo "Exporting challenge from SPKAC with ".$key." signature...\n";
+	if (function_exists('openssl_spki_export_challenge')){
+		$x = openssl_spki_export_challenge(preg_replace('/SPKAC=/', '', $spki));
+		echo $x;
+	}
+	echo "\ndone\n";
+	echo "============================\n";
+
+	echo "Exporting public key from SPKAC with ".$key." signature...\n";
+	if (function_exists('openssl_spki_export')){
+		$y = openssl_spki_export(preg_replace('/SPKAC=/', '', $spki));
+		print_r($y);
+	}
+	echo "\n============================\n";
+
+	unset($w, $x, $y, $z);
 }
-echo "\ndone\n";
-echo "============================\n";
 
-echo "Verifying SPKAC with md5 signature...\n";
-if (function_exists('openssl_spki_verify')){
- $w = openssl_spki_verify(preg_replace('/SPKAC=/', '', $spki));
- var_dump($w);
-}
-echo "\n============================\n";
-
-echo "Exporting challenge from SPKAC with md5 signature...\n";
-if (function_exists('openssl_spki_export_challenge')){
- $x = openssl_spki_export_challenge(preg_replace('/SPKAC=/', '', $spki));
- echo $x;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Exporting public key from SPKAC with md5 signature...\n";
-if (function_exists('openssl_spki_export')){
- $y = openssl_spki_export(preg_replace('/SPKAC=/', '', $spki));
- print_r($y);
-}
-echo "\n============================\n";
-
-echo "SPKAC details with md5 signature...\n";
-if (function_exists('openssl_spki_details')){
- $z = openssl_spki_details(preg_replace('/SPKAC=/', '', $spki));
- print_r($z);
-}
-echo "done\n";
-echo "============================\n";
-
-unset($w, $x, $y, $z);
-
-echo "Creating SPKAC using sha1 signature...\n";
-if (function_exists('openssl_spki_new')){
- $spki = openssl_spki_new($key, _uuid(), 'sha1');
- echo $spki;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Verifying SPKAC with sha1 signature...\n";
-if (function_exists('openssl_spki_verify')){
- $w = openssl_spki_verify(preg_replace('/SPKAC=/', '', $spki));
- var_dump($w);
-}
-echo "\n============================\n";
-
-echo "Exporting challenge from SPKAC with sha1 signature...\n";
-if (function_exists('openssl_spki_export_challenge')){
- $x = openssl_spki_export_challenge(preg_replace('/SPKAC=/', '', $spki));
- echo $x;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Exporting public key from SPKAC with sha1 signature...\n";
-if (function_exists('openssl_spki_export')){
- $y = openssl_spki_export(preg_replace('/SPKAC=/', '', $spki));
- print_r($y);
-}
-echo "\n============================\n";
-
-echo "SPKAC details with sha1 signature...\n";
-if (function_exists('openssl_spki_details')){
- $z = openssl_spki_details(preg_replace('/SPKAC=/', '', $spki));
- print_r($z);
-}
-echo "done\n";
-echo "============================\n";
-
-unset($w, $x, $y, $z);
-
-echo "Creating SPKAC using sha256 signature...\n";
-if (function_exists('openssl_spki_new')){
- $spki = openssl_spki_new($key, _uuid(), 'sha256');
- echo $spki;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Verifying SPKAC with sha256 signature...\n";
-if (function_exists('openssl_spki_verify')){
- $w = openssl_spki_verify(preg_replace('/SPKAC=/', '', $spki));
- var_dump($w);
-}
-echo "\n============================\n";
-
-echo "Exporting challenge from SPKAC with sha256 signature...\n";
-if (function_exists('openssl_spki_export_challenge')){
- $x = openssl_spki_export_challenge(preg_replace('/SPKAC=/', '', $spki));
- echo $x;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Exporting public key from SPKAC with sha256 signature...\n";
-if (function_exists('openssl_spki_export')){
- $y = openssl_spki_export(preg_replace('/SPKAC=/', '', $spki));
- print_r($y);
-}
-echo "\n============================\n";
-
-echo "SPKAC details with sha256 signature...\n";
-if (function_exists('openssl_spki_details')){
- $z = openssl_spki_details(preg_replace('/SPKAC=/', '', $spki));
- print_r($z);
-}
-echo "done\n";
-echo "============================\n";
-
-unset($w, $x, $y, $z);
-
-echo "Creating SPKAC using sha512 signature...\n";
-if (function_exists('openssl_spki_new')){
- $spki = openssl_spki_new($key, _uuid(), 'md5');
- echo $spki;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Verifying SPKAC with sha512 signature...\n";
-if (function_exists('openssl_spki_verify')){
- $w = openssl_spki_verify(preg_replace('/SPKAC=/', '', $spki));
- var_dump($w);
-}
-echo "\n============================\n";
-
-echo "Exporting challenge from SPKAC with sha512 signature...\n";
-if (function_exists('openssl_spki_export_challenge')){
- $x = openssl_spki_export_challenge(preg_replace('/SPKAC=/', '', $spki));
- echo $x;
-}
-echo "\ndone\n";
-echo "============================\n";
-
-echo "Exporting public key from SPKAC with sha512 signature...\n";
-if (function_exists('openssl_spki_export')){
- $y = openssl_spki_export(preg_replace('/SPKAC=/', '', $spki));
- print_r($y);
-}
-echo "\n============================\n";
-
-echo "SPKAC details with sha512 signature...\n";
-if (function_exists('openssl_spki_details')){
- $z = openssl_spki_details(preg_replace('/SPKAC=/', '', $spki));
- print_r($z);
-}
-echo "done\n";
-echo "============================\n";
-
-unset($w, $x, $y, $z);
-
-openssl_free_key($key);
+openssl_free_key($pkey);
 
 function _uuid()
 {
